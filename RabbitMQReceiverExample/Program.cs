b81f64716f66d93b05231aba4d898ce56e09952f
Vaclav Perlik <vaclav.perlik@gmail.com>
2020-12-07 12:01:11 +0100

    Added to GitHub

---
+++ RabbitMQReceiverExample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQReceiverExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RabbitMQReceiver.Receive("Messages", "", ExchangeType.Fanout);
+        }
+    }
+}
---
+++ RabbitMQReceiverExample/RabbitMQReceiver.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQReceiverExample
+{
+    public static class RabbitMQReceiver
+    {
+        public static ConnectionFactory factory = new ConnectionFactory()
+            { HostName = "localhost", Password = "guest", UserName = "guest", VirtualHost = "/", Port = 5672 };
+
+        public static void Receive(string exchange, string routingKey, string exchangeType)
+        {
+            var factory = new ConnectionFactory();
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: exchange, type: exchangeType);
+                var queueName = channel.QueueDeclare(exclusive: false);
+                channel.QueueBind(queue: queueName,
+                    exchange: exchange,
+                    routingKey: routingKey);
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine("Received {0}", message);
+                };
+
+                channel.BasicConsume(queue: queueName,
+                    autoAck: true,
+                    consumer: consumer);
+
+                Console.WriteLine("Waiting for messages, press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
